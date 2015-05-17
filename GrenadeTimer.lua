fbe3649539bfdbe4e5b7dd7cf83cb403bfecb495
KovaaK <kovaak@gmail.com>
2015-05-17 11:49:18 -0400

    Code clean up, +comments, prepared for release

--- GrenadeTimer.lua
+++ GrenadeTimer.lua
@@ -1,22 +1,17 @@
 require "base/internal/ui/reflexcore"
  
-local counter = 0
-local counter2 = 0
-local counter3 = 0
-local canFire = true
- 
 GrenadeTimer = {};
 registerWidget("GrenadeTimer");
 
-
-uBG = Color(255,255,255,128);
-uGradientStart = Color(255,255,255,255);
-uGradientEnd = Color(255,0,0,255);
-
+local counter, counter2, counter3 = 0, 0, 0 -- each grenade timer
+local canFire = true -- keep track of when the player can fire
+local uBG = Color(255,255,255,128); -- color of the 'clock'
+local uGradientStart = Color(255,255,255,255); -- color at top of arc for grenades
+local uGradientEnd = Color(255,0,0,255); -- color of bottom of arc for grenades
 	
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
-function SetSettings()
+function SetSettings() -- default scale/position
     consolePerformCommand("ui_set_widget_anchor GrenadeTimer 0 0");
     consolePerformCommand("ui_set_widget_offset GrenadeTimer 0 240");
     consolePerformCommand("ui_set_widget_scale GrenadeTimer 0.4");
@@ -24,9 +19,10 @@ end
 
 local function drawGrenadeArc(cnt) -- show the position of the grenade on the circle
 		nvgBeginPath();
-		nvgStrokeWidth(20);
-		local strokeLength = 0.1
-		nvgArc(0,0,120,cnt * math.pi+math.pi*1.5,(cnt+strokeLength) * math.pi+math.pi*1.5,2);
+		nvgStrokeWidth(20); -- thickness of the grenade arc
+		local strokeLength = 0.1 -- how long is the arc of each grenade
+		local offset = -0.5 -- 90 degrees back (start at top of circle)
+		nvgArc(0,0,120,(cnt+offset) * math.pi,(cnt+strokeLength+offset) * math.pi,2);
 		nvgStrokeLinearGradient(0,-80,0,80,uGradientStart,uGradientEnd);
 		nvgStroke();
 end
@@ -72,8 +68,10 @@ end
 
 function GrenadeTimer:draw()
  
+	-- pull in stored user variables
     local raceOnly = self.userData.raceOnly
     local alwaysCircle = self.userData.alwaysCircle
+	
 	if raceOnly and not isRaceMode() then return end;
 	if not shouldShowHUD() then return end;
  
@@ -82,6 +80,7 @@ function GrenadeTimer:draw()
  
 	if player.buttons.attack and player.weaponIndexSelected == 4 and canFire then
 		canFire = false
+		-- the player just fired a grenade, take the first available counter
 		if counter <= 0 then
 			counter = 2 -- fuse time
 		elseif counter2 <= 0 then
@@ -91,12 +90,15 @@ function GrenadeTimer:draw()
 		end
 	end
 
+	-- reset the 'canFire' variable if no grenade has been shot in the last 0.8 seconds
 	if counter < 1.2 and counter2 < 1.2 and counter3 < 1.2 then canFire = true end
 
+	-- draw the 'clock'
 	if alwaysCircle or counter > 0 or counter2 > 0 or counter3 > 0 then
 		drawOuterCircle();
 	end
 	
+	-- draw the grenade arcs
 	if counter > 0 then 
 		drawGrenadeArc(counter) 
 		counter = counter - deltaTime
@@ -109,6 +111,4 @@ function GrenadeTimer:draw()
 		drawGrenadeArc(counter3) 
 		counter3 = counter3 - deltaTime
 	end
-		
 end
-
